ef3acdbd6bdfbbeb5b723b8d22b4539d9ee90617
Orsell <34631691+OrsellGaming@users.noreply.github.com>
2023-01-29 13:50:35 -0700

    Improved start scripts and new Windows bat script
    Improved start script by including tmux to be used for Linux and created bat script for Windows

---
+++ run_bot.bat
@@ -0,0 +1,9 @@
+@echo off
+:: Makes sure python modules are installed using pip
+python3 -m pip install -r requirements.txt
+cls
+:: Start up the P2MM Discord Bot
+python3 p2mmbot-main.py
+:: Pause script end for user input
+echo "P2MM Discord Bot has been shutdown..."
+pause
--- run_bot.sh
+++ run_bot.sh
@@ -1,4 +1,13 @@
-# Makes sure python modules are installed using pip then starts up the bot
+# Makes sure python modules are installed using pip
 python3 -m pip install -r requirements.txt
+# Enable tmux (must be installed) to allow safe remote disconnect
+# Use "tmux attach" to return to the python session
+tmux -2 -c -u
 clear
+# Start up the P2MM Discord Bot
 python3 p2mmbot-main.py
+# Close the tmux terminal whenever the Discord Bot shuts down
+tmux C-b+x
+# Pause script end for user input
+echo "P2MM Discord Bot has been shutdown..."
+read -p "Press [Enter] key to exit..."
