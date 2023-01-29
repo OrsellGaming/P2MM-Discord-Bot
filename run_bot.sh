961fe1347c51e1a3e7354e1c9eb05e9fd915613b
Orsell <34631691+OrsellGaming@users.noreply.github.com>
2023-01-29 14:00:33 -0700

    Fixed start scripts
    Fixed start scripts

--- run_bot.sh
+++ run_bot.sh
@@ -1,13 +1,17 @@
-# Makes sure python modules are installed using pip
-python3 -m pip install -r requirements.txt
 # Enable tmux (must be installed) to allow safe remote disconnect
 # Use "tmux attach" to return to the python session
-tmux -2 -c -u
+tmux -2 -u
+
+# Makes sure python modules are installed using pip
+python3 -m pip install -r requirements.txt
 clear
+
 # Start up the P2MM Discord Bot
 python3 p2mmbot-main.py
+
 # Close the tmux terminal whenever the Discord Bot shuts down
-tmux C-b+x
+tmux kill-session
+
 # Pause script end for user input
 echo "P2MM Discord Bot has been shutdown..."
 read -p "Press [Enter] key to exit..."
