5da72ddaadb96b9ee4d3b5ef1d1a7d99df786d05
Orsell <34631691+OrsellGaming@users.noreply.github.com>
2023-01-29 12:49:38 -0700

    Fixed chmod issue with start script
    Fixed chmod issue with start script

--- run_bot.sh
+++ run_bot.sh
@@ -1,3 +1,3 @@
-# Makes sure python modules are installed and starts up the bot
+# Makes sure python modules are installed using pip then starts up the bot
 python3 -m pip install -r requirements.txt
 python3 p2mmbot-main.py
