60b19da7fce8f69b17b4f687a55d22d4ca3695fe
Orsell <34631691+OrsellGaming@users.noreply.github.com>
2023-01-31 13:50:59 -0700

    Fixed start scripts
    Fixed start scripts

--- src/run_bot.sh
+++ src/run_bot.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Makes sure python modules are installed using pip
-python3 -m pip install -r "src/requirements.txt"
+python3 -m pip install -r "requirements.txt"
 clear
 
 # Start up the P2MM Discord Bot
-python3 "src/p2mmbot-main.py"
+python3 "p2mmbot-main.py"
 
 # Pause script end for user input
 echo "P2MM Discord Bot has been shutdown..."
