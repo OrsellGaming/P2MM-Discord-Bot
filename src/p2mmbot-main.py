647a246221b4692a4510b5f982c442d6f9c0c5d1
Orsell <34631691+OrsellGaming@users.noreply.github.com>
2023-02-08 19:55:03 -0700

    Did a bunch of optimization, added Test Modal, and added additional logging
    Optimized Log directory to be created correctly along with adding more logging in places as well as adding a datetime for each log.
    
    Made sure when "testing" is disabled, all test commands are removed, and when enabled all test commands are added.
    
    Added a Test Modal, seeing what can be done with that.
    
    Changed check_admin to check for me and not the role Administrator.
    
    Added a exception check in dm_test.
    
    Made sure that only users are scanned in mod-help aren't other bots besides P2MM Bot.
    
    Fixed up on_error function for error events.

--- src/p2mmbot-main.py
+++ src/p2mmbot-main.py
@@ -8,15 +8,20 @@ from logging import handlers
 import asyncio
 import os
 import traceback
+from datetime import datetime
 
-if not os.path.exists("Logs"):
-    os.mkdir("Logs")
+basepath = os.getcwd()
+if "src" not in basepath:
+    basepath = basepath + os.sep + "src" + os.sep
+
+if not os.path.exists(basepath + "Logs"):
+    os.mkdir(basepath + "Logs")
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 handler = logging.handlers.RotatingFileHandler(
-    filename="Logs/p2mmbot.log", # Log location
+    filename=basepath + "Logs/p2mmbot.log", # Log location
     encoding="utf-8", # Log encoding
     mode="w", # Make sure when ever the bot starts it starts fresh with logs
     maxBytes=32 * 1024 * 1024,  # 32 MiB will be the max size for log files
@@ -27,28 +32,35 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 def log(msg: str) -> None:
-    print(msg)
+    now = datetime.now()
+    print(f'[{now.strftime("%d/%m/%Y %H:%M:%S")}] {msg}')
     logging.info(msg)
+
+testcommands = [
+    "dm_test",
+    "message_history_test",
+    "test_modal"
+]
 
 log("Starting the P2MM bot...")
 
 log("Grabbing config.json information...")
 
 # Get config.json
-if not os.path.exists("config.json"):
+if not os.path.exists(basepath + "config.json"):
     print("ERROR: config.json not found! config.json contains info the Discord bot needs to start! Shutting down...")
     logging.error("config.json not found! config.json contains info the Discord bot needs to start! Shutting down...")
     exit(1)
 
 try:
-    with open("config.json", "r") as config:
+    with open(basepath + "config.json", "r") as config:
         cfg = json.load(config)
 
         # Check if the user wants to use a test bot id
         if (not cfg.get("testing") is None) and (cfg["testing"]):
             token = cfg["token_testing"] # Test Bot Token
             testing_guild_id = int(cfg["testing_guild_id"]) # Testing guild id
-            log(f'"testing" is set to True, will use test bot token {testing_guild_id}...')
+            log('"testing" is set to True, will use test bot token...')
         else:
             token = cfg["token"] # Bot Token
         debug_prefix = cfg["debug_prefix"] # Bot Debug Command Prefix, Default "!"
@@ -71,12 +83,28 @@ class P2MMBot(discord.Client):
     # Runs when the bot is being setup
     async def setup_hook(self):
         log("Setting up bot hook...")
-        if cfg["testing"]:
+        if not cfg["testing"]:
+            # Copy all commands to test guild, including test commands
+            
+            try:
+                for testcommand in testcommands:
+                    self.tree.add_command(globals()[testcommand])
+                    log(f'Added test command: {testcommand}')
+            except discord.app_commands.errors.CommandAlreadyRegistered:
+                pass
+            
             self.tree.copy_global_to(guild=discord.Object(id=testing_guild_id))
-            log(f'Copied slash command to test guild "{await discord.utils.get(self.fetch_guilds(), id=testing_guild_id)}"...')
+            log(f'Copied all slash commands to test guild "{await discord.utils.get(self.fetch_guilds(), id=testing_guild_id)}"...')
         else:
-            log("Syncing slash commands to all guilds...")
-            await self.tree.sync()
+            # Remove test commands so there are not used by the general public
+            for testcommand in testcommands:
+                self.tree.remove_command(testcommand)
+                log(f'Removed test command: {testcommand}')
+                
+        log("Syncing slash commands to all guilds...")
+        for command in await self.tree.sync():
+            log(f'Command "{command}" was synced...')
+        
         log("Finished setting up bot hook...")
     
     # Runs when the bot has finished setting up
@@ -87,6 +115,43 @@ class P2MMBot(discord.Client):
         log(f'Logged on as {self.user}!')
         log("----------------------------")
 
+class Test_Modal(discord.ui.Modal, title='Test Modal'):
+    # Our modal classes MUST subclass `discord.ui.Modal`,
+    # but the title can be whatever you want.
+
+    # This will be a short input, where the user can enter their name
+    # It will also have a placeholder, as denoted by the `placeholder` kwarg.
+    # By default, it is required and is a short-style input which is exactly
+    # what we want.
+    name = discord.ui.TextInput(
+        label='Name',
+        placeholder='Type your name here... ',
+    )
+
+    # This is a longer, paragraph style input, where user can submit feedback
+    # Unlike the name, it is not required. If filled out, however, it will
+    # only accept a maximum of 300 characters, as denoted by the
+    # `max_length=300` kwarg.
+    feedback = discord.ui.TextInput(
+        label='This is the text for a textinput',
+        style=discord.TextStyle.long,
+        placeholder='Type your text here...',
+        required=False,
+        max_length=300,
+    )
+
+    async def on_submit(self, interaction: discord.Interaction):
+        log("Submitted info for the Test Modal...")
+        await interaction.response.send_message(f'Thanks for your feedback, {self.name.value}!', ephemeral=True)
+
+    async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
+        log("ERROR: Something went wrong with the Test Modal...")
+
+        # Make sure we know what the error actually is
+        log(traceback.print_exception(type(error), error, error.__traceback__))
+        await interaction.response.send_message('Oops! Something went wrong.', ephemeral=True)
+
+        
 
 intents = discord.Intents.default()
 intents.guilds = True
@@ -94,15 +159,15 @@ intents.message_content = True
 client = P2MMBot(intents=intents, command_prefix=debug_prefix)
 
 def check_admin(target_member: discord.Member) -> bool:
-    """Check if the specified user has the Administrator role.
+    """Checks if Orsell is the one to execute the command.
 
     Args:
         target_member (discord.Member): Member/User to target.
 
     Returns:
-        bool: Returns True if the user has the Administrator role.
+        bool: Returns True if the user is Orsell.
     """
-    if discord.utils.get(target_member.roles, name="Administrator"):
+    if target_member.id == 217027527602995200:
         return True
     return False
 
@@ -118,6 +183,11 @@ async def message_history_count(target_channel_id: int, target_member_id: int, l
     Returns:
         int: Total number of users message in that channel.
     """
+    log("MESSAGE HISTORY COUNT:")
+    log(f'Checking message count in channel {client.get_channel(target_channel_id)}')
+    log(f'Target user {client.get_user(target_member_id)}')
+    log(f'With limit {limit} and will print out in both console and log {output}')
+    
     message_count = 0
     async for message in client.get_channel(target_channel_id).history(limit=limit):
         if message.author.id == target_member_id:
@@ -130,7 +200,9 @@ async def message_history_count(target_channel_id: int, target_member_id: int, l
 @client.tree.command(description="Says hello back")
 async def hello(interaction: discord.Interaction):
     """Says hello!"""
-    await interaction.response.send_message(f"Hi, {interaction.user.mention}")
+
+    log("Hello command executed...")
+    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
 
 # Reponds back with when a member joinned the server
 @client.tree.command(description="Get when a user joinned the server")
@@ -142,18 +214,23 @@ async def date_joined(interaction: discord.Interaction, member: Optional[discord
     member = member or interaction.user
 
     # The format_dt function formats the date time into a human readable representation in the official client
-    await interaction.response.send_message(f"{member} joined {discord.utils.format_dt(member.joined_at)}")
+    log("Join data grabbed via slash command:")
+    log(f'{member} joined at {discord.utils.format_dt(member.joined_at)}')
+    await interaction.response.send_message(f'{member} joined {discord.utils.format_dt(member.joined_at)}')
 
 @client.tree.context_menu(name="Show Join Date")
 async def show_join_date(interaction: discord.Interaction, member: discord.Member):
+
     # The format_dt function formats the date time into a human readable representation in the official client
-    await interaction.response.send_message(f"{member} joined at {discord.utils.format_dt(member.joined_at)}", ephemeral=True)
+    log("Join data grabbed via context menu:")
+    log(f'{member} joined at {discord.utils.format_dt(member.joined_at)}')
+    await interaction.response.send_message(f'{member} joined at {discord.utils.format_dt(member.joined_at)}', ephemeral=True)
 
 @client.tree.command(description="Turns off the P2MM bot")
 async def shutdown(interaction: discord.Interaction):
     """Shutsdown the P2MM bot"""
 
-    # Check if user has the administrator role
+    # Check if user is Orsell
     if not check_admin(interaction.user):
         await interaction.response.send_message("You do not have permission to run this command!", ephemeral=True)
         return
@@ -167,7 +244,8 @@ async def shutdown(interaction: discord.Interaction):
 async def ping(interaction: discord.Interaction):
     """Pings the bot"""
 
-    embed = discord.Embed(title="Ping", description=f"Pong! {round((client.latency * 1000), 2)} ms")
+    embed = discord.Embed(title="Ping", description=f'Pong! {round((client.latency * 1000), 2)} ms', colour=discord.Colour.brand_green())
+    log("Bot has been pinged...")
     await interaction.response.send_message(embed=embed)
 
 @client.tree.command(description="Test DMing using the bot")
@@ -175,7 +253,7 @@ async def ping(interaction: discord.Interaction):
 async def dm_test(interaction: discord.Interaction, member: Optional[discord.Member] = None):
     """Test DMing using the bot"""
 
-    # Check if user has the administrator role
+    # Check if user is Orsell
     if not check_admin(interaction.user):
         await interaction.response.send_message("You do not have permission to run this command!", ephemeral=True)
         return
@@ -184,8 +262,13 @@ async def dm_test(interaction: discord.Interaction, member: Optional[discord.Mem
     member = member or interaction.user
     
     # Send the DM to the specified user
-    await member.send("Hello from the P2MM Bot!")
-    await interaction.response.send_message(f"Sent DM message to {member}")
+    try:
+        await member.send("Hello from the P2MM Bot!")
+        log((f'Sent DM message to {member}'))
+        await interaction.response.send_message(f'Sent DM message to {member}')
+    except discord.Forbidden:
+        log(f"Failed to DM message to {member} as they have blocked the bot or have server DM's disabled.")
+        await interaction.response.send_message(f"Failed to DM message to {member} as they have blocked the bot or have server DM's disabled.")
 
 @client.tree.command(description="Grab the specified users last 50 messages or how many there are if less than 50 are present.")
 @app_commands.describe(
@@ -203,7 +286,7 @@ async def message_history_test(interaction: discord.Interaction, channel: str, m
             "The specified user to get the last 50 messages; defaults to the user who uses the command."
     """
 
-    # Check if user has the administrator role
+    # Check if user is Orsell
     if not check_admin(interaction.user):
         await interaction.response.send_message("You do not have permission to run this command!", ephemeral=True)
         return
@@ -211,10 +294,13 @@ async def message_history_test(interaction: discord.Interaction, channel: str, m
     # If no member is explicitly provided then we use the command user here
     member = member or interaction.user
     
+    log(f'Grabbing messages of user: {member}')
+
     # Check to make sure the specified channel is valid/exists
     target_channel = discord.utils.get(client.get_all_channels(), name=channel)
     if target_channel == None:
-        await interaction.response.send_message(f"Failed to grab the messages of \"{member}\" in the specified channel \"{channel}\" as it doesn't exist.")
+        log(f'Failed to grab the messages of "{member}" in the specified channel "{channel}" as it doesn\'t exist.')
+        await interaction.response.send_message(f'Failed to grab the messages of "{member}" in the specified channel "{channel}" as it doesn\'t exist.')
         return  
     
     # Count up the number of messages the author sent
@@ -222,30 +308,43 @@ async def message_history_test(interaction: discord.Interaction, channel: str, m
     
     # If there are 0 messages in the channel it will report as so
     if message_count == 0:
-        await interaction.response.send_message(f"No messages of user \"{member}\" were found in \"{channel}\"")
+        log(f'No messages of user "{member}" were found in "{channel}"')
+        await interaction.response.send_message(f'No messages of user "{member}" were found in "{channel}"')
         return
     
-    await interaction.response.send_message(f"Grabbed last {message_count} messages of \"{member}\" in \"{channel}\", check console for history.")
+    log(f'Grabbed last {message_count} messages of "{member}" in "{channel}", check console for history.')
+    await interaction.response.send_message(f'Grabbed last {message_count} messages of "{member}" in "{channel}", check console for history.')
+
+@client.tree.command(description="Test the Test Modal")
+async def test_modal(interaction: discord.Interaction):
+
+    # Check if user is Orsell
+    if not check_admin(interaction.user):
+        await interaction.response.send_message("You do not have permission to run this command!", ephemeral=True)
+        return
+
+    log("Test Modal was tested...")
+    await interaction.response.send_modal(Test_Modal())
 
 # Called whenever someone sends a message
 @client.event
-async def on_message(message: discord.Message):
+async def on_message(message: discord.Message, interaction: discord.Interaction):
     if (message.author.id == client.user.id):
         return
     
-    #log(f"Message from id {message.author.id}, author name {message.author}, in channel {message.channel}, with id {message.channel.id}: {message.content}")
-    if (message.channel.id == mod_help_channel_id) and (await message_history_count(mod_help_channel_id, message.author.id, output=None) == 1):
-        await message.reply(
-            "Hey there! It appears it's your first time messaging in <#839751998445846568>!\nMake sure to check out the <#1027963220851429387>, your answer might be there!\nMessage will delete in 15 seconds...", 
-            mention_author=True, 
-            delete_after=15
-        )
+    #log(f'Message from id {message.author.id}, author name {message.author}, in channel {message.channel}, with id {message.channel.id}: {message.content}")
+    if (message.channel.id == mod_help_channel_id) and (await message_history_count(mod_help_channel_id, message.author.id, output=None) == 1) and (not message.author.bot):
+        log(f'New comer in #mod-help! User: {message.author} ID: {message.author.id}')
+        await interaction.response.send_message(
+            "Hey there! It appears it's your first time messaging in <#839751998445846568>!\nMake sure to check out the <#1027963220851429387>, your answer might be there!", 
+            ephemeral=True)
         return
     
     if not message.content.startswith(debug_prefix):
         return
     
     if "hello" in message.content:
+        log("Hello command executed...")
         await message.reply("Hello!", mention_author=True)
 
 # @client.event
@@ -253,9 +352,9 @@ async def on_message(message: discord.Message):
 #     client.get_channel(discord.utils.get(predicate, iterable))
 
 @client.event
-async def on_error(event:str, *args, **kwargs):
-    log(f"An non-fatal error occured! With event: {event}")
-    log(traceback.format_exc())
+async def on_error(self, interaction: discord.Interaction, error: Exception, event:str):
+    log(f'An non-fatal error occured! With event: {event}')
+    log(traceback.print_exception(type(error), error, error.__traceback__))
     await client.get_channel(1062430492558888980).send("<@217027527602995200> ERROR: An error occured with the bot! Check the console!")
 
 # START THE BOT!!!
